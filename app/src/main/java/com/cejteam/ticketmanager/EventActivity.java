2b5dfdb4e4aef970f58bf8c6697e6daa9af6d243
JoelRamiro <joel.caballero@grupoleitz.com>
2017-05-09 16:57:52 -0600

    Primeros cambios a la actividad de eventos

---
+++ app/src/main/java/com/cejteam/ticketmanager/EventActivity.java
@@ -0,0 +1,41 @@
+package com.cejteam.ticketmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventActivity extends AppCompatActivity {
+  Spinner spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+        spinner= (Spinner)findViewById(R.id.spinner);
+        List list= new ArrayList();
+        list.add("Deportivo");
+        list.add("Musical");
+        list.add("Religioso");
+        ArrayAdapter arrayAdapter= new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line,list);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+}
