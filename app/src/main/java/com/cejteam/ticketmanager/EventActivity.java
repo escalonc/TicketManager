26852c821e6dbfc814bbc98aa2aed80523fb0d46
JoelRamiro <joel.caballero@grupoleitz.com>
2017-05-19 11:45:07 -0600

    Avances pantalla crear evento

--- app/src/main/java/com/cejteam/ticketmanager/EventActivity.java
+++ app/src/main/java/com/cejteam/ticketmanager/EventActivity.java
@@ -1,24 +1,57 @@
 package com.cejteam.ticketmanager;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.support.annotation.IntDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
-
+import android.support.v7.app.ActionBarActivity;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class EventActivity extends AppCompatActivity {
-  Spinner spinner;
-    @Override
+  private Spinner spinner;
+    private int año,mes,dia;
+    private EditText campofecha;
+    private Button guardar;
+    private static  final int tipo_dialogo=0;
+    private static DatePickerDialog.OnDateSetListener oyenteSelectorFecha;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
 
+        campofecha=(EditText)findViewById(R.id.dateeventr);
+        Calendar calendar= Calendar.getInstance();
+        año= calendar.get(Calendar.YEAR);
+        mes= calendar.get(Calendar.MONTH);
+        dia= calendar.get(Calendar.DAY_OF_MONTH);
+        mostrarfecha();
+        oyenteSelectorFecha = new  DatePickerDialog.OnDateSetListener(){
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                año=year;
+                mes=month;
+                dia=dayOfMonth;
+                mostrarfecha();
+            }
+
+        };
+
+
+
         spinner= (Spinner)findViewById(R.id.event_type);
+
 
         List list= new ArrayList();
         list.add("Deportivo");
@@ -42,4 +75,27 @@ public class EventActivity extends AppCompatActivity {
         });
 
     }
+
+    public void mostrarfecha(){
+        campofecha.setText(dia+"/"+(mes+1)+"/"+año);
+    }
+
+    @Override
+    protected  Dialog onCreateDialog(int id){
+        switch (id){
+            case 0:
+                return new DatePickerDialog(this, oyenteSelectorFecha, año, mes, dia);
+
+            /*
+            POR SI LA FECHA APARECE EN 1900 INICIALIZADA
+            return new DatePickerDialog(this,oyenteSelectorFecha,año,mes,dia)
+             */
+        }
+        return null;
+    }
+
+    public void mostrarCalendar(View control){
+        showDialog(tipo_dialogo);
+    }
+
 }
