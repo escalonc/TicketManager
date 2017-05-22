9a96e8c913e05fc95b7309f43641922b02530209
Christopher Escalon <chrisescalon@gmail.com>
2017-05-22 12:39:18 -0600

    Reparacion de activity crear evento. falta verificar si almacena

--- app/src/main/java/com/cejteam/ticketmanager/EventActivity.java
+++ app/src/main/java/com/cejteam/ticketmanager/EventActivity.java
@@ -23,10 +23,10 @@ import java.util.List;
 import static com.cejteam.ticketmanager.R.id.dateeventr;
 
 public class EventActivity extends AppCompatActivity implements View.OnClickListener {
-  private Spinner spinner;
+    private Spinner spinner;
     private int año,mes,dia;
-    private Button guardar;
-    private static  final int tipo_dialogo=0;
+        private Button guardar;
+        private static  final int tipo_dialogo=0;
     private static DatePickerDialog.OnDateSetListener oyenteSelectorFecha;
     ArrayList<EventActivity> items;
 
@@ -48,18 +48,14 @@ public class EventActivity extends AppCompatActivity implements View.OnClickList
 
 
     protected void onCreate(Bundle savedInstanceState) {
-
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
-        Calendar calendar= Calendar.getInstance();
+  /*      Calendar calendar= Calendar.getInstance();
         año= calendar.get(Calendar.YEAR);
         mes= calendar.get(Calendar.MONTH);
         dia= calendar.get(Calendar.DAY_OF_MONTH);
         mostrarfecha();
-        oyenteSelectorFecha = new  DatePickerDialog.OnDateSetListener(){
+        /*oyenteSelectorFecha = new  DatePickerDialog.OnDateSetListener(){
 
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
@@ -70,7 +66,7 @@ public class EventActivity extends AppCompatActivity implements View.OnClickList
             }
 
         };
-
+*/
 
 
         spinner= (Spinner)findViewById(R.id.event_type);
@@ -103,7 +99,7 @@ public class EventActivity extends AppCompatActivity implements View.OnClickList
         campofecha.setText(dia+"/"+(mes+1)+"/"+año);
     }
 
-    @Override
+   /* @Override
     protected  Dialog onCreateDialog(int id){
         switch (id){
             case 0:
@@ -112,11 +108,11 @@ public class EventActivity extends AppCompatActivity implements View.OnClickList
             /*
             POR SI LA FECHA APARECE EN 1900 INICIALIZADA
             return new DatePickerDialog(this,oyenteSelectorFecha,año,mes,dia)
-             */
+
         }
         return null;
     }
-
+*/
     public void mostrarCalendar(View control){
         showDialog(tipo_dialogo);
     }
@@ -152,7 +148,6 @@ public class EventActivity extends AppCompatActivity implements View.OnClickList
 
 
     }
-
 
 
 
--- app/src/main/java/com/cejteam/ticketmanager/LoginActivity.java
+++ app/src/main/java/com/cejteam/ticketmanager/LoginActivity.java
@@ -51,7 +51,7 @@ public class LoginActivity extends AppCompatActivity {
         button.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent ListSong = new Intent(getApplicationContext(), EventActivity.class);
+                Intent ListSong = new Intent(getApplicationContext(), Create_EventActivity.class);
                 startActivity(ListSong);
             }
         });
